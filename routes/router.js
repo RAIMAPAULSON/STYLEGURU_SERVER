92dc912ec375bb0ccecc3dd3167a41853d462fa7
RAIMAPAULSON <raimapaulson11@gmail.com>
2024-08-01 00:54:53 +0530

    completed

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productController')
 const userController = require('../controllers/userController')
 const jwtMiddleware = require('../middlewares/jwtMiddleware')
 const wishlistController = require('../controllers/wishlistController')
+const cartController = require('../controllers/cartController')
 const router = new express.Router()
 // get all products
 router.get('/all-products',productController.getAllProductsController)
@@ -18,6 +19,18 @@ router.post('/addToWishlist',jwtMiddleware,wishlistController.addToWishlist)
 router.get('/get-wishlist',jwtMiddleware,wishlistController.getWishlist)
 // wishlist remove
 router.delete('/wishlist/:id/remove',jwtMiddleware,wishlistController.removeWishlistItem)
+// addtocart
+router.post('/addToCart',jwtMiddleware,cartController.addToCart)
+// getcart
+router.get('/get-cart',jwtMiddleware,cartController.getCart)
+// removecartitems
+router.delete('/cart/:id/remove',jwtMiddleware,cartController.removeCartItem)
+// incrementcart
+router.get('/cart/:id/increment',jwtMiddleware,cartController.incrementCart)
+// decrementcart
+router.get('/cart/:id/decrement',jwtMiddleware,cartController.decrementCart)
+// emptycart
+router.delete('/empty-cart',jwtMiddleware,cartController.emptyCart)
 
 
 
